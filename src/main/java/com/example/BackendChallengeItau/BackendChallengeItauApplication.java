d4cb39c569a07b27dd53b4b29d49da208df64817
Patrick <patrickmacedo02@gmail.com>
2026-02-03 15:44:54 -0300

    Project created.

---
+++ src/main/java/com/example/BackendChallengeItau/BackendChallengeItauApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendChallengeItau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengeItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackendChallengeItau/BackendChallengeItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendChallengeItau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
